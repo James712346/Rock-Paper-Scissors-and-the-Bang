ee494a9838cf4b46629609cc182afe128444c5ed
james712346 <james712346@gmail.com>
2019-06-03 14:06:10 +1000

    Adding some more comments

--- RockPaperScissorsandtheBANG.py
+++ RockPaperScissorsandtheBANG.py
@@ -14,7 +14,7 @@ except:
 """
 .place(relx=(1/len(self.Moves)*self.Player.Human.Buttons.index(Button))+1/len(self.Moves)/2
 """
-
+Moves =  ["Rock","Paper","Scissors", "Spock", "Lizard"] # make a Global list of moves
 
 class Move(tk.Frame):
     def __init__(self,parent,name,index,bind,image,tall = 0,disabled = False, *args, **kwargs):
@@ -50,14 +50,13 @@ class Move(tk.Frame):
         pass
 class Player(tk.Frame):
     def __init__(self):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
+        tk.Frame.__init__(self, parent, *args, **kwargs) #Setup the main Frame (Class, root, [and any other arguments that could be called])
 
 class PSRLS(tk.Frame):
-    def __init__(self, parent,*args, **kwargs):
+    def __init__(self,parent,*args, **kwargs):
         tk.Frame.__init__(self, parent,*args, **kwargs) #Setup the main Frame (Class, root, [and any other arguments that could be called])
         self.parent = parent # sets the root from where this class is being run from to self.parent
         self.Time = 0 # Sets self.time for the wait function to 0
-        _move =  ["Rock","Paper","Scissors", "Spock", "Lizard"] # make a temperary list of move
         if not "psrls.txt" in os.listdir(): #Check in the psrls file is in the programs directory
             #if it isn't then it makes the file
             with open("psrls.txt","w") as file:
@@ -72,12 +71,11 @@ class PSRLS(tk.Frame):
             _comtall = [int(tall) for tall in file[1].strip().split(",")] #graps the second line and puts into a list of comtall (these are temperary variables)
             self.winstats = file[2].strip().split(",") #make a list out of the third line and set it to a self.winstats
             _binds = file[3].strip().split(",") #sets the last line to a temperary variable of binds
-        _pictures = self.GetPictures() #calls a class function of GetPictures which will either grab the pictures from the local directory or from the internet (my github repository)
+        _pictures, self.Global_Pictures = self.GetPictures() #calls a class function of GetPictures which will either grab the pictures from the local directory or from the internet (my github repository) and return two list on of the moves pictures and the other of the Global Pictures
 
         for i in range(0,5):
-            Move(self, _move[i], i, _binds[i], _pictures[i]).pack()
-
-
+            Move(self, Moves[i], i, _binds[i], _pictures[i]).pack()
+            
     def Wait(self, t, Global=False):
         if Global:
             self.Time = time.time() + t
@@ -88,22 +86,22 @@ class PSRLS(tk.Frame):
         return
 
     def GetPictures(self):
-        __ImgNames =  ["Rock","Paper","Scissors", "Spock", "Lizard","i_icon","setting_icon","Waiting"] #temperary variable for the names of pictures that need to be added
-        __ImgObjects = []
+        _ImgNames = Moves+["i_icon","setting_icon","Waiting"] #compinds two list together (List of moves, Other Pictures) to make a temperary variable for the names of pictures that need to be added
+        _ImgObjects = [] #temperary variable for the functions/objects of pictures that will be returned once this function is over
         print("Loading Pictures...")
-        for Img in _ImgNames:
+        for Img in _ImgNames: #Loop over all the name of pictures
             try:
-                _ImgObjects.append(ImageTk.PhotoImage(Image.open(Img+".png")))
-            except:
+                _ImgObjects.append(ImageTk.PhotoImage(Image.open(Img+".png"))) #grabs the local picture
+            except: # if an error occurs then the program will atempt to grab the online version
                 try:
-                    import urllib.request as urllib
-                    import io
-                    _ImgObjects.append(ImageTk.PhotoImage(Image.open(io.BytesIO(urllib.urlopen("https://raw.githubusercontent.com/James712346/Rock-Paper-Scissors-and-the-Bang/master/"+Img+".png").read()))))
+                    import urllib.request as urllib #imports the urllib to grab the picture from te url
+                    import io #imports the io library to convert the data from urllib to readable data for pillow library
+                    _ImgObjects.append(ImageTk.PhotoImage(Image.open(io.BytesIO(urllib.urlopen("https://raw.githubusercontent.com/James712346/Rock-Paper-Scissors-and-the-Bang/master/"+Img+".png").read())))) #Grabs the Pictures from my github repository
                 except:
-                    _ImgObjects.append("Failed")
-                    print("Failed to load",Img+".png")
+                    _ImgObjects.append("Failed") #if the program fails to grab the image then it appends and prints into the console that the file has filed
+                    print("Failed to load",Img+".png") #also when it time to display these images the program will note that the picture failed to load and will display text instead of the img
         print("Finished Loading pictures")
-        return _ImgObjects
+        return _ImgObjects[:-3], _ImgObjects[-3:]  #returns the ImgObjects
 
     def UpdateFile(self):
         with open("psrls.txt","w") as file:
